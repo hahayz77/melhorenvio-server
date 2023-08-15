071cd3e7b3fa48d2ec5e2c725f74165b37a42170
hahayz77 <danilo__danilo@hotmail.com>
2023-08-15 09:31:02 -0300

    shipment calculation OK

---
+++ src/functions/shipment/Calculate.js
@@ -0,0 +1,44 @@
+const shipmentCalculate = async (access_token, zip_code) => {
+    try {
+        const response = await fetch("https://sandbox.melhorenvio.com.br/api/v2/me/shipment/calculate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Bearer ${access_token}`,
+                "User-Agent": "Aplicação e-mail"
+            }, body: JSON.stringify({
+                "from": {
+                    "postal_code": "01002001"
+                },
+                "to": {
+                    "postal_code": "90570020"
+                },
+                "package": {
+                    "height": 4,
+                    "width": 12,
+                    "length": 17,
+                    "weight": 0.3
+                },
+                "options": {
+                    "insurance_value": 1180.87,
+                    "receipt": false,
+                    "own_hand": false
+                },
+                "services": "1,2,3,4,7,11"
+            })
+        })
+
+        // console.log(response);
+        if (!response.ok) throw new Error (response.statusText);
+
+        const data = await response.json();
+        // console.log(data);
+        return data;
+
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+module.exports = shipmentCalculate;
--- src/routes/shipment.js
+++ src/routes/shipment.js
@@ -2,16 +2,13 @@ require('dotenv').config();
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
+const shipmentCalculate = require('../functions/shipment/Calculate');
 
 router.use(authMiddleware);
 
 router.get("/", async (req, res) => {
     try {
         const { access_token } = req.body;
-        // console.log(req.body)
-
-        
-
 
         res.status(200).json({ access_token });
     } catch (err) {
@@ -21,11 +18,12 @@ router.get("/", async (req, res) => {
 
 router.post('/calculate', async (req, res) => {
     try {
-        // const { zipCode } = req.body;
+        const { access_token, zip_code } = req.body;
+        const data = await shipmentCalculate(access_token, zip_code);
 
         // if(!zipCode) return res.status(400).json({msg: "ZipCode not found!"})
 
-        res.status(200).json({ zipCode: zipCode });
+        res.status(200).json({ data });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
