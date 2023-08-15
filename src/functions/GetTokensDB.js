47534b713de7adaa913051fc1bc608b760b2d290
hahayz77 <danilo__danilo@hotmail.com>
2023-08-15 09:31:48 -0300

    getTokensDB correction throw error

--- src/functions/GetTokensDB.js
+++ src/functions/GetTokensDB.js
@@ -3,6 +3,7 @@ const Token = require("../database/models/Token");
 const getTokensDB = async () => {
     try {
         const getTokens = await Token.findOne();
+        if(!getTokens) throw new Error("error token db find")
         return getTokens;
 
     } catch (err) {
