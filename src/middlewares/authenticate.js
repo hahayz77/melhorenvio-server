95052331ca7eb20da281821a0c205bbf6fa58bcb
hahayz77 <danilo__danilo@hotmail.com>
2023-08-29 08:10:28 -0300

    new authenticate middleware

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -6,28 +6,33 @@ const refreshToken = require('../functions/RefreshToken');
 
 module.exports = async (req, res, next) => {
     try {
+        let decoded;
         const currentTimestamp = Math.floor(Date.now() / 1000);
         let { access_token } = req.body; // if access_token was sended from the client
-        if (!access_token) throw new Error("No token was provided!")  // If no one access_token was sended from the client
 
-        const decoded = await jwt.decode(access_token); //decode token
-        if (!decoded) throw new Error("Error on Decode Token! Something is wrong with this token.");
+        if (access_token) {
+            decoded = await jwt.decode(access_token); //decode token from db
+            if(!decoded) throw new Error("Error on Decode Token! Something is wrong with this stored token.");
+        }
+        
 
-
-        if (currentTimestamp < decoded.exp) { // < to force refresh
+        // < expired || > not expired || Wrong token ?
+        if (currentTimestamp > decoded?.exp || !access_token) {
             const tokensFromDB = await getTokensDB();
             if (!tokensFromDB) throw new Error("Internal server error! No token provided from storage.");
+            access_token = tokensFromDB.access_token;
 
-            const decodeTokenFromDB = await jwt.decode(tokensFromDB.access_token); //decode token from db
+            const decodeTokenFromDB = await jwt.decode(access_token); //decode token from db
             if (!decodeTokenFromDB) throw new Error("Error on Decode Token! Something is wrong with this stored token.");
 
-            if (currentTimestamp < decodeTokenFromDB.exp) {
+
+            if (currentTimestamp > decodeTokenFromDB.exp) { // < expired || > not expired                
                 access_token = await refreshToken(tokensFromDB.refresh_token);
                 if (!access_token) throw new Error("Internal Server Error! No Access Token from storage!");
             }
         }
         // All OK! No need to refresh tokens
-        req.body.access_token = access_token; //send access_token back to the next route
+        req.body.access_token = access_token; //send access_token back to the next route if its a new one taken from the server or DB
         return next();
     } catch (err) {
         next(err);
